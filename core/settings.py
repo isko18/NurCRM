e76f6ee544c030929445f47304abcfe6007d9199
isko18 <toksonbaevislam2004@gmail.com>
2025-06-16 20:47:35 +0300

    prod

--- core/settings.py
+++ core/settings.py
@@ -155,9 +155,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
