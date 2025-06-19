43f05e04294591f6f7714b0a8137e107b11b0c02
isko18 <toksonbaevislam2004@gmail.com>
2025-06-19 23:24:51 +0300

    prof

--- apps/main/signals.py
+++ apps/main/signals.py
@@ -1,3 +1,5 @@
+# apps/main/signals.py
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from apps.main.models import Task
@@ -6,4 +8,5 @@ from apps.main.tasks import create_task_notification
 @receiver(post_save, sender=Task)
 def notify_assigned_user_async(sender, instance, created, **kwargs):
     if created and instance.assigned_to:
+        # Мы вызываем задачу только после того, как задача была сохранена
         create_task_notification.delay(str(instance.id))
