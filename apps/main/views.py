ecb2f59f3f7dcd4dc8df38fa566c4e808b71974e
isko18 <toksonbaevislam2004@gmail.com>
2025-06-23 00:13:50 +0600

    add filters

--- apps/main/views.py
+++ apps/main/views.py
@@ -18,7 +18,6 @@ from apps.main.serializers import (
 )
 
 
-# Универсальный миксин для всех вьюх
 class CompanyRestrictedMixin:
     permission_classes = [permissions.IsAuthenticated]
 
@@ -32,13 +31,12 @@ class CompanyRestrictedMixin:
         serializer.save(company=self.request.user.company)
 
 
-# Контакты
 class ContactListCreateAPIView(CompanyRestrictedMixin, generics.ListCreateAPIView):
     serializer_class = ContactSerializer
     queryset = Contact.objects.all()
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['name', 'email', 'phone', 'client_company']
-    filterset_fields = ['department']
+    filterset_fields = '__all__'
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user, company=self.request.user.company)
@@ -49,12 +47,12 @@ class ContactRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.Retri
     queryset = Contact.objects.all()
 
 
-# Воронки
 class PipelineListCreateAPIView(CompanyRestrictedMixin, generics.ListCreateAPIView):
     serializer_class = PipelineSerializer
     queryset = Pipeline.objects.all()
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     search_fields = ['name']
+    filterset_fields = '__all__'
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user, company=self.request.user.company)
@@ -65,13 +63,12 @@ class PipelineRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.Retr
     queryset = Pipeline.objects.all()
 
 
-# Сделки
 class DealListCreateAPIView(CompanyRestrictedMixin, generics.ListCreateAPIView):
     serializer_class = DealSerializer
     queryset = Deal.objects.all()
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['title', 'stage']
-    filterset_fields = ['status', 'pipeline', 'assigned_to', 'contact']
+    filterset_fields = '__all__'
 
 
 class DealRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.RetrieveUpdateDestroyAPIView):
@@ -79,13 +76,12 @@ class DealRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.Retrieve
     queryset = Deal.objects.all()
 
 
-# Задачи
 class TaskListCreateAPIView(CompanyRestrictedMixin, generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['title', 'description']
-    filterset_fields = ['status', 'assigned_to', 'deal', 'due_date']
+    filterset_fields = '__all__'
 
 
 class TaskRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.RetrieveUpdateDestroyAPIView):
@@ -93,12 +89,11 @@ class TaskRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.Retrieve
     queryset = Task.objects.all()
 
 
-# Интеграции
 class IntegrationListCreateAPIView(CompanyRestrictedMixin, generics.ListCreateAPIView):
     serializer_class = IntegrationSerializer
     queryset = Integration.objects.all()
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['type', 'status']
+    filterset_fields = '__all__'
 
 
 class IntegrationRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.RetrieveUpdateDestroyAPIView):
@@ -106,38 +101,35 @@ class IntegrationRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.R
     queryset = Integration.objects.all()
 
 
-# Аналитика
 class AnalyticsListAPIView(CompanyRestrictedMixin, generics.ListAPIView):
     serializer_class = AnalyticsSerializer
     queryset = Analytics.objects.all()
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['type']
+    filterset_fields = '__all__'
 
 
-# Заказы
 class OrderListCreateAPIView(CompanyRestrictedMixin, generics.ListCreateAPIView):
     serializer_class = OrderSerializer
-    queryset = Order.objects.all().prefetch_related('items__product')  # 🔍 подтягиваем товары
+    queryset = Order.objects.all().prefetch_related('items__product')
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['order_number', 'customer_name', 'department', 'phone']
-    filterset_fields = ['status', 'date_ordered']
+    filterset_fields = '__all__'
 
     def perform_create(self, serializer):
-        # company уже устанавливается в сериализаторе
         serializer.save()
+
 
 class OrderRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.RetrieveUpdateDestroyAPIView):
     serializer_class = OrderSerializer
     queryset = Order.objects.all().prefetch_related('items__product')
 
 
-# Продукты
 class ProductListCreateAPIView(CompanyRestrictedMixin, generics.ListCreateAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['name', 'article', 'brand__name', 'category__name']
-    filterset_fields = ['category', 'brand']
+    filterset_fields = '__all__'
 
 
 class ProductRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.RetrieveUpdateDestroyAPIView):
@@ -145,12 +137,11 @@ class ProductRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.Retri
     queryset = Product.objects.all()
 
 
-# Отзывы
 class ReviewListCreateAPIView(CompanyRestrictedMixin, generics.ListCreateAPIView):
     serializer_class = ReviewSerializer
     queryset = Review.objects.all()
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['rating', 'user']
+    filterset_fields = '__all__'
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user, company=self.request.user.company)
@@ -161,12 +152,11 @@ class ReviewRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.Retrie
     queryset = Review.objects.all()
 
 
-# Уведомления
 class NotificationListView(CompanyRestrictedMixin, generics.ListAPIView):
     serializer_class = NotificationSerializer
     queryset = Notification.objects.all()
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['is_read']
+    filterset_fields = '__all__'
 
 
 class NotificationDetailView(CompanyRestrictedMixin, generics.RetrieveAPIView):
@@ -182,30 +172,27 @@ class MarkAllNotificationsReadView(APIView):
         return Response({'status': 'Все уведомления прочитаны'}, status=status.HTTP_200_OK)
 
 
-# События
 class EventListCreateAPIView(CompanyRestrictedMixin, generics.ListCreateAPIView):
     serializer_class = EventSerializer
     queryset = Event.objects.all()
     filter_backends = [DjangoFilterBackend]
+    filterset_fields = '__all__'
 
-    def get_serializer_context(self):
-        return {'request': self.request}
 
 class EventRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EventSerializer
     queryset = Event.objects.all()
 
-    def get_serializer_context(self):
-        return {'request': self.request}
 
 class WarehouseListCreateAPIView(CompanyRestrictedMixin, generics.ListCreateAPIView):
     serializer_class = WarehouseSerializer
     queryset = Warehouse.objects.all()
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    search_fields = ['name', 'location']  # Для поиска по имени склада и его расположению
+    search_fields = ['name', 'location']
+    filterset_fields = '__all__'
 
     def perform_create(self, serializer):
-        serializer.save(company=self.request.user.company)  # Присваиваем склад компании текущего пользователя
+        serializer.save(company=self.request.user.company)
 
 
 class WarehouseRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.RetrieveUpdateDestroyAPIView):
@@ -213,39 +200,42 @@ class WarehouseRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.Ret
     queryset = Warehouse.objects.all()
 
 
-# Складское событие (WarehouseEvent)
 class WarehouseEventListCreateAPIView(CompanyRestrictedMixin, generics.ListCreateAPIView):
     serializer_class = WarehouseEventSerializer
     queryset = WarehouseEvent.objects.all()
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    search_fields = ['title', 'client_name']  # Поиск по названию события и клиенту
+    search_fields = ['title', 'client_name']
+    filterset_fields = '__all__'
 
     def perform_create(self, serializer):
-        serializer.save(company=self.request.user.company)  # Присваиваем событие компании текущего пользователя
+        serializer.save(company=self.request.user.company)
 
 
 class WarehouseEventRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.RetrieveUpdateDestroyAPIView):
     serializer_class = WarehouseEventSerializer
     queryset = WarehouseEvent.objects.all()
-    
-    
+
+
 class ProductCategoryListCreateAPIView(CompanyRestrictedMixin, generics.ListCreateAPIView):
     serializer_class = ProductCategorySerializer
     queryset = ProductCategory.objects.all()
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['name']
+    filterset_fields = '__all__'
+
 
 class ProductCategoryRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductCategorySerializer
     queryset = ProductCategory.objects.all()
 
 
-# Бренды товаров
 class ProductBrandListCreateAPIView(CompanyRestrictedMixin, generics.ListCreateAPIView):
     serializer_class = ProductBrandSerializer
     queryset = ProductBrand.objects.all()
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['name']
+    filterset_fields = '__all__'
+
 
 class ProductBrandRetrieveUpdateDestroyAPIView(CompanyRestrictedMixin, generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductBrandSerializer
