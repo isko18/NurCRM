43a6bfb4ee6a963e8e66ab16c6ac46aca84b4ece
isko18 <toksonbaevislam2004@gmail.com>
2025-08-30 21:55:39 +0600

    fix urls

--- apps/booking/admin.py
+++ apps/booking/admin.py
@@ -1,9 +1,57 @@
 from django.contrib import admin
-from .models import ConferenceRoom, Booking, ManagerAssignment, Hotel
-
-admin.site.register(ConferenceRoom)
-admin.site.register(Booking)
-admin.site.register(ManagerAssignment)
-admin.site.register(Hotel)
+from .models import Hotel, Bed, ConferenceRoom, Booking, ManagerAssignment, Folder, Document
 
 
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ("name", "company", "capacity", "price")
+    list_filter = ("company",)
+    search_fields = ("name", "description")
+    ordering = ("company", "name")
+
+
+@admin.register(Bed)
+class BedAdmin(admin.ModelAdmin):
+    list_display = ("name", "company", "capacity", "price")
+    list_filter = ("company",)
+    search_fields = ("name", "description")
+    ordering = ("company", "name")
+
+
+@admin.register(ConferenceRoom)
+class ConferenceRoomAdmin(admin.ModelAdmin):
+    list_display = ("name", "company", "capacity", "location", "price")
+    list_filter = ("company",)
+    search_fields = ("name", "location")
+    ordering = ("company", "name")
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("id", "company", "hotel", "room", "bed", "reserved_by", "start_time", "end_time", "purpose")
+    list_filter = ("company", "start_time", "end_time")
+    search_fields = ("purpose", "reserved_by__email", "hotel__name", "room__name", "bed__name")
+    date_hierarchy = "start_time"
+    ordering = ("-start_time",)
+
+
+@admin.register(ManagerAssignment)
+class ManagerAssignmentAdmin(admin.ModelAdmin):
+    list_display = ("company", "room", "manager")
+    list_filter = ("company",)
+    search_fields = ("room__name", "manager__email")
+
+
+@admin.register(Folder)
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ("name", "company", "parent")
+    list_filter = ("company",)
+    search_fields = ("name",)
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ("name", "company", "folder", "created_at", "updated_at")
+    list_filter = ("company", "created_at", "updated_at")
+    search_fields = ("name", "file")
+    ordering = ("name",)
