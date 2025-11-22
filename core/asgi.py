4b2ffc185f6ef16aad3ebaa531701011440c0979
isko18 <toksonbaevislam2004@gmail.com>
2025-11-22 22:03:50 +0600

    firts commit

--- core/asgi.py
+++ core/asgi.py
@@ -8,14 +8,22 @@ django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
+
 from apps.instagram.ws_jwt import JWTAuthMiddleware
 from apps.instagram import routing as ig_routing
+from apps.scale import ws_routing as scale_ws_routing  # <-- добавили
+
+# объединяем все ws-маршруты в один список
+websocket_urlpatterns = (
+    ig_routing.websocket_urlpatterns
+    + scale_ws_routing.websocket_urlpatterns
+)
 
 application = ProtocolTypeRouter({
-    "http": ASGIStaticFilesHandler(django_asgi_app),  # <-- добавляем статику
+    "http": ASGIStaticFilesHandler(django_asgi_app),
     "websocket": AllowedHostsOriginValidator(
         JWTAuthMiddleware(
-            URLRouter(ig_routing.websocket_urlpatterns)
+            URLRouter(websocket_urlpatterns)
         )
     ),
 })
