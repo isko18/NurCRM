8bec6874ea978066d7d176c0ecc18844220c800a
isko18 <toksonbaevislam2004@gmial.com>
2026-06-16 00:27:12 +0600

    f1

--- warehouse_service/apps/warehouse/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class WarehouseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.warehouse'
-
-    def ready(self):
-        from . import signals  # noqa: F401 — регистрация обработчиков
--- warehouse_service/apps/warehouse/filters/__init__.py
+++
@@ -1,9 +0,0 @@
-
-
-from .warehouse import WarehouseFilter
-
-from .brand import BrandFilter
-
-from .category import CategoryFilter
-
-from .product import ProductFilter
