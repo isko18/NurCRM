87bb52eef578931736dbd9acea916b701606c792
Fondante-GGG <grazy9891@gmail.com>
2026-07-27 14:19:00 +0600

    fix(warehouse): add missing PageNumberPagination import

--- apps/warehouse/views_documents.py
+++ apps/warehouse/views_documents.py
@@ -1,4 +1,5 @@
 from rest_framework import status, permissions, filters
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
