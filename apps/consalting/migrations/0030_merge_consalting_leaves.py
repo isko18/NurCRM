1f440ef6817fe02aaa9896187e09c4f2066b9f95
Fondante-GGG <grazy9891@gmail.com>
2026-08-31 00:36:50 +0600

    fix(consalting): merge migration leaves

---
+++ apps/consalting/migrations/0030_merge_consalting_leaves.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('consalting', '0020_whatsappmessageconsalting_content_uri_and_more'),
+        ('consalting', '0029_bonusruleconsalting_bonustierconsalting_and_more'),
+    ]
+
+    operations = [
+    ]
